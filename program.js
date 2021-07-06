990d8ab1dba5c53e2e9ff2f286aa8629cdbf64a7
wills-designs <william3696@live.com>
2021-07-05 22:03:21 -0400

    updated names, added inputs for age/gender

--- program.js
+++ program.js
@@ -1,1 +1,12 @@
-console.log(11);
+const identity = {
+    _Names: ["Liam", "Olivia", "Noah", "Emma", "Oliver", "Ava","William","Sophia","Elijah","Isabella", "James", "Charlotte", "Benjamin",
+    "Amelia",
+    "Lucas",
+    "Mia",
+    "Mason",
+    "Harper",
+    "Ethan",
+    "Evelyn"],
+    _Age: [25, 23, 15, 20, 24, 10, 32, 45, 100, 92, 52, 22, 76],
+    _gender: ['male', 'female', 'non-gender']
+}
