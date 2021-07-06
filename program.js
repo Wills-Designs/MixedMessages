9be2de3192fd50905a278c3e58c25b928008193e
wills-designs <william3696@live.com>
2021-07-05 22:12:05 -0400

    added getters and setters for name, age, gender

--- program.js
+++ program.js
@@ -1,5 +1,5 @@
 const identity = {
-    _Names: ["Liam", "Olivia", "Noah", "Emma", "Oliver", "Ava","William","Sophia","Elijah","Isabella", "James", "Charlotte", "Benjamin",
+    _names: ["Liam", "Olivia", "Noah", "Emma", "Oliver", "Ava","William","Sophia","Elijah","Isabella", "James", "Charlotte", "Benjamin",
     "Amelia",
     "Lucas",
     "Mia",
@@ -7,6 +7,25 @@ const identity = {
     "Harper",
     "Ethan",
     "Evelyn"],
-    _Age: [25, 23, 15, 20, 24, 10, 32, 45, 100, 92, 52, 22, 76],
-    _gender: ['male', 'female', 'non-gender']
+    _age: [25, 23, 15, 20, 24, 10, 32, 45, 100, 92, 52, 22, 76],
+    _gender: ['male', 'female', 'non-gender'],
+    get name() {
+        return this._names;
+    },
+    get age() {
+        return this._age;
+    },
+    get gender() {
+        return this._gender;
+    },
+    set name(input) {
+        typeof input === 'string' ? this._names.push(input) : console.log('Name must be in string format!');
+    },
+    set age(input) {
+        typeof input === 'number' ? this._age.push(input) : console.log('Age must be a number!');
+    },
+    set gender(input) {
+        typeof input === 'string' ? this._gender.push(input) : console.log('Gender must be in string format!');
+    }
 }
+
